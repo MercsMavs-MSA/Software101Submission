9f165947fb58c37d8a26f0ac2f571d15d5634c64
JathinB <battepatijathin@gmail.com>
2026-07-16 20:49:45 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/Wheel/WheelSubsystem.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.Wheel;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.Constants;
+
+public class WheelSubsystem {
+    //TODO: FIX MOTOR ID
+    private final TalonFX wheelMotor = new TalonFX(Constants.WHEEL_MOTOR_ID);
+
+    private final VelocityVoltage request = new VelocityVoltage(0);
+
+    public WheelSubsystem()
+    {
+        TalonFXConfiguration cfg = new TalonFXConfiguration();
+        cfg.Slot0.kV = 0;
+        cfg.Slot0.kP = 0;
+        cfg.CurrentLimits.StatorCurrentLimit = 60;
+        cfg.CurrentLimits.StatorCurrentLimitEnable = true;
+        cfg.Feedback.SensorToMechanismRatio = 
+        wheelMotor.getConfigurator().apply(cfg);
+    }
+
+    /**
+     * Set velocity of the wheelMotor
+     * @param rps Desired velocity in rotations per second
+     */
+    public void setVelocity(double rps)
+    {
+        wheelMotor.setControl(request.withVelocity(rps));
+    }
+}
