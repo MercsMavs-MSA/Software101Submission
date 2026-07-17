e1544e66bfc76112c751ccb591b887bd795912ee
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2026-07-16 20:54:53 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/arm/SpindexerConstants.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.arm;
+
+import com.ctre.phoenix6.signals.InvertedValue;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class SpindexerConstants {
+  public static InvertedValue INVERTED = InvertedValue.Clockwise_Positive;
+  public static double GEAR_RATIO = 10.0;
+  public static double SPINDEXER_KV = 1;
+  public static int SPINDEXER_MOTOR_ID = 43;
+  public static int SUPPLY_LIMIT = 30;
+  public static int STATOR_LIMIT = 60;
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/arm/SpindexerSubsystem.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.arm;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SpindexerSubsystem extends SubsystemBase {
+  private TalonFX spindexerMotor = new TalonFX(SpindexerConstants.SPINDEXER_MOTOR_ID);
+
+  private final VelocityVoltage request = new VelocityVoltage(0.0);
+
+  /** Creates a new SpindexerSubsystem. */
+  public SpindexerSubsystem() {
+    TalonFXConfiguration spindexerConfiguration = new TalonFXConfiguration();
+    spindexerConfiguration.Slot0.kV = SpindexerConstants.SPINDEXER_KV;
+    spindexerConfiguration.Feedback.RotorToSensorRatio = SpindexerConstants.GEAR_RATIO;
+    spindexerConfiguration.CurrentLimits.StatorCurrentLimit = SpindexerConstants.STATOR_LIMIT;
+    spindexerConfiguration.CurrentLimits.SupplyCurrentLimit = SpindexerConstants.SUPPLY_LIMIT;
+    spindexerConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+    spindexerConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+    spindexerConfiguration.MotorOutput.Inverted = SpindexerConstants.INVERTED;
+    spindexerMotor.getConfigurator().apply(spindexerConfiguration);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void run(double rps) {
+    spindexerMotor.setControl(request.withVelocity(rps));
+  }
+
+  public void stop() {
+    spindexerMotor.stopMotor();
+  }
+}
