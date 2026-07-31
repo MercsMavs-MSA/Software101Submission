33f547efb7c93fa32eab8c15d6e548830e11e51c
Jaxson West <jaxson.w@mckinneysteamacademy.org>
2026-07-31 03:05:10 -0500

    Binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Wheel.WheelConstants;
 import frc.robot.subsystems.Wheel.WheelSubsystem;
 
 /**
@@ -24,6 +25,8 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(0);
+
+  private final TeleopCommands teleopComs = new TeleopCommands(wheelSubsystem, new WheelConstants());
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -52,6 +55,10 @@ public class RobotContainer {
     
     m_driverController.leftTrigger().onFalse(slowCommand);
     m_driverController.rightTrigger().onFalse(slowCommand);
+
+    //I'm using the bumpers because the last slide is unfinished and the triggers are in use already
+    m_driverController.leftBumper().onTrue(teleopComs.seqRangeWheel());
+    m_driverController.rightBumper().onTrue(teleopComs.parrCommand());
     
   }
 
