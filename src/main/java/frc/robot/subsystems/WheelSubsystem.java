c7e9d4ebadb2d71e70d6e7b143aa40ef86d57f4c
videepgmsa <videep.g@mckinneysteamacademy.org>
2026-08-26 18:29:01 -0500

    Initial

---
+++ src/main/java/frc/robot/subsystems/WheelSubsystem.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class WheelSubsystem extends SubsystemBase {
+  private final TalonFX wheelMotor =
+    new TalonFX (Constants.WHEEL_MOTOR_ID);
+  private final VelocityVoltage request = 
+    new VelocityVoltage (0);
+
+public WheelSubsystem() {
+  var cfg = new TalonFXConfiguration();
+  cfg.Slot0.kP = Constants.WHEEL_KP;
+  cfg.Slot0.kD = Constants.WHEEL_KD;
+  cfg.Slot0.kG = Constants.WHEEL_KP;
+  wheelMotor.getConfigurator().apply(cfg);
+}
+
+public void goToVelocity (double rotations) {
+  wheelMotor.setControl (
+    request.withAcceleration (rotations));
+}
+}
